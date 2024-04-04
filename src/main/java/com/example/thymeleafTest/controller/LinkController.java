9bfc326083df3d787cb9b85fdce79d7d23821223
FINE <finelee1119@gmail.com>
2024-04-04 16:23:53 +0900

    Commit change

--- src/main/java/com/example/thymeleafTest/controller/LinkController.java
+++ src/main/java/com/example/thymeleafTest/controller/LinkController.java
@@ -1,4 +1,21 @@
 package com.example.thymeleafTest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LinkController {
+    @GetMapping("/mylink/link")
+    public String linkTest(Model model) {
+        model.addAttribute("name", "장원영");
+        model.addAttribute("age", 21);
+        return "/link/linkView";
+    }
+
+    @GetMapping("hello")
+    public String helloPage() {
+        return "/link/hello_page";
+    }
 }
+
